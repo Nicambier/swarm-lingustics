cd4270bddcd067183406557d6045d17c1220d957
Nicambier <nicolas.cambier@utc.fr>
2017-11-17 16:38:55 +0100

    Script for cluster

--- argos3-language/experiments/experiment.sh
+++ argos3-language/experiments/experiment.sh
@@ -1,53 +1,55 @@
 #!/bin/bash
+mkdir /tmp/vtrianni/swarm_results
 mkdir results
 
 pr="$(nproc)"
 pr="$((pr-1))"
+    
+FILE='aggregation_auto.argos'
 
-for i in $(seq 1 1 50)
+for version in 'n' 'w' 's'
 do
-    mkdir results/"$i"/
+    sed -e 's/library="..\/build\/controllers\/[^\"]*"/library="..\/build\/controllers\/footbot_aggregation_'"$version"'NG\/libfootbot_aggregation_'"$version"'NG.so"/' $FILE > tmp1
+    sed -e 's/footbot_aggregation.*controller/footbot_aggregation_'"$version"'NG_controller/' tmp1 > tmp2
     
-    FILE='aggregation_auto.argos'
-    
-    for version in 'n' 'w' 's'
+    for swarm in 30 100 200
     do
-        sed -e 's/library="..\/build\/controllers\/[^\"]*"/library="..\/build\/controllers\/footbot_aggregation_'"$version"'NG\/libfootbot_aggregation_'"$version"'NG.so"/' $FILE > tmp1
-        sed -e 's/footbot_aggregation.*controller/footbot_aggregation_'"$version"'NG_controller/' tmp1 > tmp2
-        
-        for swarm in 30 100 200
+        sed -e 's/<entity quantity=".*" max_trials="100">/<entity quantity="'"$swarm"'" max_trials="100">/' tmp2 > tmp3
+    
+        a=1
+        for aParam in 4.6 2.3 1.5 1.15 0.9 0.75 0.65
         do
-            sed -e 's/<entity quantity=".*" max_trials="100">/<entity quantity="'"$swarm"'" max_trials="100">/' tmp2 > tmp3
-        
-            a=1
-            for aParam in 4.6 2.3 1.5 1.15 0.9 0.75 0.65 0.6
+            sed -e 's/aParam="[^\"]*"/aParam="'"$aParam"'"/' tmp3 > tmp4
+            
+            b=1
+            for bParam in 4.6 2.3 1.5 1.15 0.9 0.75 0.65
             do
-                sed -e 's/aParam="[^\"]*"/aParam="'"$aParam"'"/' tmp3 > tmp4
+                sed -e 's/bParam="[^\"]*"/bParam="'"$bParam"'"/' tmp4 > tmp5
                 
-                b=1
-                for bParam in 4.6 2.3 1.5 1.15 0.9 0.75 0.65 0.6
+                mkdir /tmp/vtrianni/swarm_results/"$version"NG_s"$swarm"_a"$a"_b"$b"
+                
+                for i in $(seq 1 1 50)
                 do
-                    sed -e 's/bParam="[^\"]*"/bParam="'"$bParam"'"/' tmp4 > tmp5
-                    
-                    sed -e 's/output="[^\"]*"/output="results\/'"$i"'\/'"$version"'NG_s'"$swarm"'_a'"$a"'_b'"$b"'.txt"/'  tmp5 > tmp.argos
-                    argos3 -c tmp.argos &
+                    sed -e 's/output="[^\"]*"/output="/tmp/vtrianni/swarm_results\/'"$version"'NG_s'"$swarm"'_a'"$a"'_b'"$b"'\/'"$i"'.txt"/'  tmp5 > tmp.argos
+                    qsub run.sh -N "$version"NG_s"$swarm"_a"$a"_b"$b"_"$i"
         
-                    T="$(pgrep -c argos3)"
-                    while [ "$T" -gt "$pr" ]; do
-                        sleep 10
-                        T="$(pgrep -c argos3)"
-                    done
-                    
-                    b="$((b+1))"
-                done
-            a="$((a+1))"
+                    #T="$(pgrep -c argos3)"
+                    #while [ "$T" -gt "$pr" ]; 
+                    #do
+                    #    sleep 10
+                    #    T="$(pgrep -c argos3)"
+                    #done
+                done                
+                b="$((b+1))"
             done
+        a="$((a+1))"
         done
     done
-    if [ "$(pgrep -c argos3)" -lt 1 ]
-    then 
-        echo "done"
-        rm tmp*
-    fi
-    
 done
+
+#if [ "$(pgrep -c argos3)" -lt 1 ]
+#then 
+#    cp -rf /tmp/swarm_results results/
+#    echo "done"
+#    rm tmp*
+#fi
---
+++ argos3-language/experiments/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#$ -cwd
+argos3 -c tmp.argos $
